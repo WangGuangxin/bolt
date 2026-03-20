prestodb/presto

fix: Fix NaN values in Spark collect_set aggregate function (#12335)
Summary:
In Spark collect_set aggregate function, NaN values are considered distinct.
```
spark.sql("SELECT collect_set(col) FROM VALUES (1), (1), (cast('NaN' as double)), (cast('NaN' as double)) AS tab(col)").show()
+----------------+
|collect_set(col)|
+----------------+
| [1.0, NaN, NaN]|
+----------------+
```
Fixes https://github.com/facebookincubator/velox/issues/12336.

Pull Request resolved: https://github.com/facebookincubator/velox/pull/12335

Reviewed By: Yuhta

Differential Revision: D71740373

Pulled By: xiaoxmeng

fbshipit-source-id: 90bf22162aebae89e8df5b99ff359d9fc87d92e1

velox/functions/sparksql/aggregates/CollectSetAggregate.cpp
@@ -24,6 +24,14 @@ namespace {
 template <typename T>
 using SparkSetAggAggregate = SetAggAggregate<T, true, false>;
 
+// NaN inputs are treated as distinct values.
+template <typename T>
+using FloatSetAggAggregateNaNUnaware = SetAggAggregate<
+    T,
+    true,
+    false,
+    velox::aggregate::prestosql::FloatSetAccumulatorNaNUnaware<T>>;
+
 } // namespace
 
 void registerCollectSetAggAggregate(
@@ -72,9 +80,11 @@ void registerCollectSetAggAggregate(
                 "Non-decimal use of HUGEINT is not supported");
             return std::make_unique<SparkSetAggAggregate<int128_t>>(resultType);
           case TypeKind::REAL:
-            return std::make_unique<SparkSetAggAggregate<float>>(resultType);
+            return std::make_unique<FloatSetAggAggregateNaNUnaware<float>>(
+                resultType);
           case TypeKind::DOUBLE:
-            return std::make_unique<SparkSetAggAggregate<double>>(resultType);
+            return std::make_unique<FloatSetAggAggregateNaNUnaware<double>>(
+                resultType);
           case TypeKind::TIMESTAMP:
             return std::make_unique<SparkSetAggAggregate<Timestamp>>(
                 resultType);

WangGuangxin/bolt

bolt/functions/lib/aggregates/CollectSetAggregate.cpp
@@ -39,6 +39,14 @@ namespace {
 template <typename T>
 using SparkSetAggAggregate = SetAggAggregate<T, true, false>;
 
+// NaN inputs are treated as distinct values.
+template <typename T>
+using FloatSetAggAggregateNaNUnaware = SetAggAggregate<
+    T,
+    true,
+    false,
+    bolt::aggregate::prestosql::FloatSetAccumulatorNaNUnaware<T>>;
+
 } // namespace
 
 void registerCollectSetAggregate(
@@ -86,9 +94,11 @@ void registerCollectSetAggregate(
                 "Non-decimal use of HUGEINT is not supported");
             return std::make_unique<SparkSetAggAggregate<int128_t>>(resultType);
           case TypeKind::REAL:
-            return std::make_unique<SparkSetAggAggregate<float>>(resultType);
+            return std::make_unique<FloatSetAggAggregateNaNUnaware<float>>(
+                resultType);
           case TypeKind::DOUBLE:
-            return std::make_unique<SparkSetAggAggregate<double>>(resultType);
+            return std::make_unique<FloatSetAggAggregateNaNUnaware<double>>(
+                resultType);
           case TypeKind::TIMESTAMP:
             return std::make_unique<SparkSetAggAggregate<Timestamp>>(
                 resultType);