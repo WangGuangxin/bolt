prestodb/presto

fix: Untangle some strange code in `PlanBuilder::finalAggregation()` (#12466)
Summary:
The problematic code was brought in https://github.com/facebookincubator/velox/issues/1419.

The code block is actually

```cpp
if (!a) {
  VELOX_CHECK(a);
}
```

which is apparently wrong. The code was intentionally to find an aggregation with `Step::kPartial`. The patch recovers the right
 readable logic.

Pull Request resolved: https://github.com/facebookincubator/velox/pull/12466

Reviewed By: kagamiori

Differential Revision: D71768609

Pulled By: xiaoxmeng

fbshipit-source-id: 4da81c6f7ddba2fc550da30ee95c286d9618c81c

velox/exec/tests/utils/PlanBuilder.cpp
@@ -805,14 +805,11 @@ PlanBuilder& PlanBuilder::finalAggregation() {
   if (!exec::isRawInput(aggNode->step())) {
     // If aggregation node is not the partial aggregation, keep looking again.
     aggNode = findPartialAggregation(aggNode->sources()[0].get());
-    if (!exec::isRawInput(aggNode->step())) {
-      VELOX_CHECK_NOT_NULL(
-          aggNode,
-          "Plan node before current plan node must be a partial aggregation.");
-      VELOX_CHECK(exec::isRawInput(aggNode->step()));
-      VELOX_CHECK(exec::isPartialOutput(aggNode->step()));
-    }
+    VELOX_CHECK_NOT_NULL(aggNode);
   }
+
+  VELOX_CHECK(exec::isRawInput(aggNode->step()));
+  VELOX_CHECK(exec::isPartialOutput(aggNode->step()));
 
   auto step = core::AggregationNode::Step::kFinal;
 

WangGuangxin/bolt

bolt/exec/tests/utils/PlanBuilder.cpp
@@ -783,14 +783,11 @@ PlanBuilder& PlanBuilder::finalAggregation(const bool useGPU) {
   if (!exec::isRawInput(aggNode->step())) {
     // If aggregation node is not the partial aggregation, keep looking again.
     aggNode = findPartialAggregation(aggNode->sources()[0].get());
-    if (!exec::isRawInput(aggNode->step())) {
-      BOLT_CHECK_NOT_NULL(
-          aggNode,
-          "Plan node before current plan node must be a partial aggregation.");
-      BOLT_CHECK(exec::isRawInput(aggNode->step()));
-      BOLT_CHECK(exec::isPartialOutput(aggNode->step()));
-    }
+    BOLT_CHECK_NOT_NULL(aggNode);
   }
+
+  BOLT_CHECK(exec::isRawInput(aggNode->step()));
+  BOLT_CHECK(exec::isPartialOutput(aggNode->step()));
 
   auto step = core::AggregationNode::Step::kFinal;
 