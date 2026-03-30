prestodb/presto

fix: Fix reading table with non-ascii names (#12606)
Summary:
`folly::toLowerAscii` only works for ascii characters, which causes
inconsistency when `fileColumnNamesReadAsLowerCase` is set as true
where non-ascii names in the `fileType` are still in upper case while
the schema is in lower case. This PR uses `stringImpl::lower` to
convert UTF-8 string as lower case in the DWRF and Parquet readers.

Fixes https://github.com/facebookincubator/velox/issues/11092.

Pull Request resolved: https://github.com/facebookincubator/velox/pull/12606

Reviewed By: Yuhta

Differential Revision: D76279107

Pulled By: kKPulla

fbshipit-source-id: 91f8cbac875cfc0360a483261e6a4b12a942c821

velox/dwio/parquet/reader/ParquetReader.cpp
@@ -21,6 +21,7 @@
 #include "velox/dwio/parquet/reader/ParquetColumnReader.h"
 #include "velox/dwio/parquet/reader/StructColumnReader.h"
 #include "velox/dwio/parquet/thrift/ThriftTransport.h"
+#include "velox/functions/lib/string/StringImpl.h"
 
 namespace facebook::velox::parquet {
 
@@ -304,7 +305,7 @@ std::unique_ptr<ParquetTypeWithId> ReaderBase::getParquetColumnInfo(
 
   auto name = schemaElement.name;
   if (isFileColumnNamesReadAsLowerCase()) {
-    folly::toLowerAscii(name);
+    name = functions::stringImpl::utf8StrToLowerCopy(name);
   }
 
   if ((!options_.useColumnNamesForColumnMapping()) &&
@@ -331,7 +332,7 @@ std::unique_ptr<ParquetTypeWithId> ReaderBase::getParquetColumnInfo(
       ++schemaIdx;
       auto childName = schema[schemaIdx].name;
       if (isFileColumnNamesReadAsLowerCase()) {
-        folly::toLowerAscii(childName);
+        childName = functions::stringImpl::utf8StrToLowerCopy(childName);
       }
 
       TypePtr childRequestedType = nullptr;
@@ -977,7 +978,7 @@ std::shared_ptr<const RowType> ReaderBase::createRowType(
   for (auto& child : children) {
     auto childName = static_cast<const ParquetTypeWithId&>(*child).name_;
     if (fileColumnNamesReadAsLowerCase) {
-      folly::toLowerAscii(childName);
+      childName = functions::stringImpl::utf8StrToLowerCopy(childName);
     }
     childNames.push_back(std::move(childName));
     childTypes.push_back(child->type());

WangGuangxin/bolt

bolt/dwio/parquet/reader/ParquetReader.cpp
@@ -44,6 +44,7 @@
 #include "bolt/dwio/parquet/thrift/ThriftInternal.h"
 #include "bolt/dwio/parquet/thrift/ThriftTransport.h"
 #include "bolt/dwio/parquet/thrift/codegen/parquet_types.h"
+#include "bolt/functions/lib/string/StringImpl.h"
 
 namespace bytedance::bolt::parquet {
 
@@ -431,7 +432,7 @@ std::shared_ptr<const ParquetTypeWithId> ReaderBase::getParquetColumnInfo(
 
   auto name = schemaElement.name;
   if (isFileColumnNamesReadAsLowerCase()) {
-    folly::toLowerAscii(name);
+    name = functions::stringImpl::utf8StrToLowerCopy(name);
   }
   if (!schemaElement.__isset.type) { // inner node
     BOLT_CHECK(
@@ -448,7 +449,7 @@ std::shared_ptr<const ParquetTypeWithId> ReaderBase::getParquetColumnInfo(
       ++schemaIdx;
       auto childName = schema[schemaIdx].name;
       if (isFileColumnNamesReadAsLowerCase()) {
-        folly::toLowerAscii(childName);
+        childName = functions::stringImpl::utf8StrToLowerCopy(childName);
       }
 
       TypePtr childRequestedType = nullptr;
@@ -998,7 +999,7 @@ std::shared_ptr<const RowType> ReaderBase::createRowType(
     auto childName =
         std::static_pointer_cast<const ParquetTypeWithId>(child)->name_;
     if (fileColumnNamesReadAsLowerCase) {
-      folly::toLowerAscii(childName);
+      childName = functions::stringImpl::utf8StrToLowerCopy(childName);
     }
     childNames.push_back(std::move(childName));
     childTypes.push_back(child->type());